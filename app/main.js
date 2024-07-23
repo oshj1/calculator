d8c5ecf9554fbb12814121accc756abb3d81d723
ddr <mrdrifter123@outlook.com>
2024-07-23 19:01:12 +1000

    added function calls to operate()

--- app/main.js
+++ app/main.js
@@ -1,3 +1,7 @@
+let Number1 = 0;
+let Number2 = 0;
+let Operation = "";
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -16,14 +20,18 @@ function divide(num1, num2){
 
 function operate(operator, num1, num2){
     if (operator === "+"){
-
+        let result = add(num1, num2);
     } 
 
     else if (operator === "-"){
-
+        let result = subtract(num1, num2);
     } 
 
     else if (operator === "/"){
+        let result = divide(num1, num2);
+    }
 
+    else if (operator === "*"){
+        let result = multiply(num1, num2);
     }
 }
